55d33adc59eb1cb7a4847ccc092d015af7f9b84b
matheus <nanamprogrammer@gmail.com>
2021-04-26 05:36:46 -0300

    refact dotenv import just for conde styling

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { db } from "./database";
 import { routes } from "./routes";
-require("dotenv").config();
+// require("dotenv").config();
+import { config as dotenvConfig } from "dotenv";
 const app = express();
 const PORT = process.env.NODE_ENV_PORT || 3000;
 
@@ -12,6 +13,7 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => console.log("Database connected!"));
 
 (function expressConfig() {
+  dotenvConfig();
   app.use(express.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
